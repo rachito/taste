37f54b6d9db36d953e75612edb094eec0ed08298
Rajiv Rivero <rajiv.rivero@gmail.com>
2013-02-26 22:18:10 -0600

    Estructura de directorios basada en "Two Scoops Django Best Practices"

---
+++ taste/principal/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from taste.principal.models import UserProfile, Business, Branch, Post, Menu, MenuSection, MenuItem
+
+admin.site.register(UserProfile)
+admin.site.register(Business)
+admin.site.register(Branch)
+admin.site.register(Post)
+admin.site.register(Menu)
+admin.site.register(MenuSection)
+admin.site.register(MenuItem)
+
